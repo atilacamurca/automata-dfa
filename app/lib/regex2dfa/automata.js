9ec20ce4f9fe48140becb3029c888144ff8eb82d
Átila Camurça <camurca.home@gmail.com>
2014-11-13 17:07:23 -0300

    Still working...

--- app/lib/regex2dfa/automata.js
+++ app/lib/regex2dfa/automata.js
@@ -61,20 +61,84 @@ Automata.prototype.addTransition = function (fromState, toState, input) {
     }
 };
 
+Automata.prototype.addTransitionDict = function (transitions) {
+    var fromState = transitions.keys();
+    var toStates = transitions.values();
+    var lenFromState = fromState.length;
+    //var lenToStates = toStates.length;
+    for (var i = 0; i < lenFromState; i++) {
+        var state = toStates[i];
+        var keys = state.keys();
+        var len = keys.length;
+        for (var j = 0; j < len; j++) {
+            // TODO verificar possíveis problemas com esse método!!!!!!!!!
+            this.addTransition(fromState[i], keys[j], state.get(keys[i]));
+        }
+    }
+};
+
+Automata.prototype.getTransitions = function (state, key) {
+    if (_.isNumber(state)) {
+        state = [state];
+    }
+
+    var transitions = new Set();
+    var len = state.length;
+    for (var i = 0; i < len; i++) {
+        var st = state[i];
+        var keys = this.transitions.keys();
+        if (_.contains(keys, st)) {
+            var values = this.transitions.values();
+            var lenValues = values.length;
+            for (var j = 0; j < lenValues; j++) {
+                var tns = values[j];
+                var setKeys = tns.keys();
+                if (_.contains(setKeys, key)) {
+                    transitions.add(tns);
+                }
+            }
+        }
+    }
+    return transitions;
+};
+
+Automata.prototype.getEClose = function (findState) {
+    var allStates = new Set();
+    var states = new Set([findState]);
+    var keys = states.keys();
+    var len = keys.length;
+    while (len !== 0) {
+        var state = states.pop();
+        allStates.add(state);
+        var trKeys = this.transitions.keys();
+        if (_.contains(trKeys, state)) {
+            var values = this.transitions.values();
+            var lenValues = values.length;
+            for (var j = 0; j < lenValues; j++) {
+                var tns = trKeys[j];
+                var aux = values[j];
+                var setKeys = aux.keys();
+                if (setKeys.has(Automata.epsilon()) &&
+                    ! _.contains(allStates, tns)) {
+                    states.add(tns);
+                }
+            }
+        }
+    }
+    return allStates;
+};
+
 Automata.prototype.display = function () {
     console.log('states:', this.states);
     console.log('start state:', this.startState);
     console.log('final states:', this.finalStates);
-    console.log('transitions:', this.transitions);
-    /*var fromStates = this.transitions.keys();
-    var lenFromStates = fromStates.length;
-    for (var i = 0; i < lenFromStates; i++) {
-        console.log(this.transitions.get(fromStates[i]));
-        var toStates = fromStates[i].keys();
-        var lenToStates = toStates.length;
-        for (var j = 0; j < lenToStates; j++) {
-            console.log('    ', toStates[j]);
-        }
-        console.log(''); // new line
-    }*/
+    console.log('transitions:');
+
+    var fromState = this.transitions.keys();
+    var toStates = this.transitions.values();
+    var lenToStates = toStates.length;
+    for (var i = 0; i < lenToStates; i++) {
+        var state = toStates[i];
+        console.log(fromState[i], state);
+    }
 };
